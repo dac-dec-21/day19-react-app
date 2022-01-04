fbf70752020e59d82950bd2c7af6a6dcb616af15
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-04 11:21:14 +0530

    delete tweet.

--- src/App.js
+++ src/App.js
@@ -18,11 +18,23 @@ function MyComponent() {
     setList(newList);
   };
 
+  const deleteFirstTweet = () => {
+    list.splice(0, 1);
+
+    const newList = [...list];
+    setList(newList);
+  };
+
   return (
     <div>
       <h1>Working with Input Element</h1>
 
       <input type="button" value="Tweet to Everyone" onClick={tweetHere} />
+      <input
+        type="button"
+        value="Delete First Tweet"
+        onClick={deleteFirstTweet}
+      />
 
       <div id="parent">
         {list.map((item) => (
