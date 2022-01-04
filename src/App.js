6ee0366575551220dd76398c3ea5169229b18726
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-04 09:26:22 +0530

    b4.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function App() {
   return (
     <>
@@ -8,19 +10,39 @@ export default function App() {
 
 function MyComponent() {
   const id1 = 100;
-  const background = "bg-info";
+  const list = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+  const [cardCss, setCardcss] = useState("bg-primary text-light fs-3 p-4 mb-1");
+
+  const primaryTheme = () => setCardcss("bg-primary text-light fs-3 p-4 mb-1");
+  const darkTheme = () => setCardcss("bg-dark text-light fs-3 p-4 mb-1");
+  const dangerTheme = () => setCardcss("bg-danger text-light fs-3 p-4 mb-1");
 
   return (
     <div>
-      <h1>Interpolation {id1}</h1>
-      <h1>Interpolation {id1 + 100}</h1>
-
-      <h1 id="1" className="bg-primary">
-        Interpolation {id1}
-      </h1>
-      <h1 id={id1} className={background}>
-        Interpolation {id1}
-      </h1>
+      <input
+        type="button"
+        value="Primary Theme"
+        className="btn btn-primary btn-sm"
+        onClick={primaryTheme}
+      />
+      <input
+        type="button"
+        value="Primary Theme"
+        className="btn btn-dark btn-sm"
+        onClick={darkTheme}
+      />
+      <input
+        type="button"
+        value="Danger Theme"
+        className="btn btn-danger btn-sm"
+        onClick={dangerTheme}
+      />
+      {list.map(() => (
+        <div className={cardCss}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur,
+          earum. {cardCss}
+        </div>
+      ))}
     </div>
   );
 }
