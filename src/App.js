e7ecfa155eef820e1fd72f6b6be4d6063c7f289f
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-04 11:31:09 +0530

    b9

--- src/App.js
+++ src/App.js
@@ -9,32 +9,35 @@ export default function App() {
 }
 
 function MyComponent() {
-  const [list, setList] = useState(["Hello Worldddd"]);
+  const [message, setMessage] = useState("Tweet here...");
+  const [list, setList] = useState([]);
 
   const tweetHere = () => {
-    const newList = [...list, "Hello Universe"];
+    const newList = [message, ...list];
 
     // UI RERENDER :: innerHTML
     setList(newList);
+    setMessage("");
   };
 
-  const deleteFirstTweet = () => {
-    list.splice(0, 1);
+  const changeMessage = (e) => {
+    const newMessage = e.target.value;
+    setMessage(newMessage);
+  };
 
-    const newList = [...list];
-    setList(newList);
+  const deleteTweet = () => {
+    list.splice(0, 1);
+    setList([...list]);
   };
 
   return (
     <div>
       <h1>Working with Input Element</h1>
 
+      <input type="text" value={message} onChange={changeMessage} />
+
       <input type="button" value="Tweet to Everyone" onClick={tweetHere} />
-      <input
-        type="button"
-        value="Delete First Tweet"
-        onClick={deleteFirstTweet}
-      />
+      <input type="button" value="Tweet to Everyone" onClick={deleteTweet} />
 
       <div id="parent">
         {list.map((item) => (
