ba81ebb16abc9a61b0ee4976421033ef02109e45
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-04 09:38:20 +0530

    interpoliation continue...

--- src/App.js
+++ src/App.js
@@ -9,40 +9,20 @@ export default function App() {
 }
 
 function MyComponent() {
-  const id1 = 100;
-  const list = [1, 1, 1, 1, 1, 1, 1, 1, 1];
-  const [cardCss, setCardcss] = useState("bg-primary text-light fs-3 p-4 mb-1");
+  const id = 1;
+  const msg = "Hello World, How are you?";
+  const btnMsg = "Click Me!!!";
 
-  const primaryTheme = () => setCardcss("bg-primary text-light fs-3 p-4 mb-1");
-  const darkTheme = () => setCardcss("bg-dark text-light fs-3 p-4 mb-1");
-  const dangerTheme = () => setCardcss("bg-danger text-light fs-3 p-4 mb-1");
+  const hello = () => {
+    console.log("Click me! Function Called!");
+    alert("Thank you");
+  };
 
   return (
     <div>
-      <input
-        type="button"
-        value="Primary Theme"
-        className="btn btn-primary btn-sm"
-        onClick={primaryTheme}
-      />
-      <input
-        type="button"
-        value="Primary Theme"
-        className="btn btn-dark btn-sm"
-        onClick={darkTheme}
-      />
-      <input
-        type="button"
-        value="Danger Theme"
-        className="btn btn-danger btn-sm"
-        onClick={dangerTheme}
-      />
-      {list.map(() => (
-        <div className={cardCss}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur,
-          earum. {cardCss}
-        </div>
-      ))}
+      <div id={id}>{msg}</div>
+
+      <input type="button" value={btnMsg} onClick={hello} />
     </div>
   );
 }
