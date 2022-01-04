7fb012d6d0d3a84b8ff55eb2c742a441a788901f
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-04 11:07:58 +0530

    b8

--- src/App.js
+++ src/App.js
@@ -9,13 +9,20 @@ export default function App() {
 }
 
 function MyComponent() {
-  const [list] = useState(["Hello Worldddd"]);
+  const [list, setList] = useState(["Hello Worldddd"]);
+
+  const tweetHere = () => {
+    const newList = [...list, "Hello Universe"];
+
+    // UI RERENDER :: innerHTML
+    setList(newList);
+  };
 
   return (
     <div>
       <h1>Working with Input Element</h1>
 
-      <input type="button" value="Tweet to Everyone" />
+      <input type="button" value="Tweet to Everyone" onClick={tweetHere} />
 
       <div id="parent">
         {list.map((item) => (
