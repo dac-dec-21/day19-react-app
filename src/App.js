1b0f08586639b57e3a8fdf61366e20b1ab007f9c
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-04 19:20:43 +0530

    ajax.

--- src/App.js
+++ src/App.js
@@ -4,118 +4,29 @@ export default function App() {
   return (
     <>
       <MyComponent />
-      <MyComponent />
     </>
   );
 }
 
 function MyComponent() {
-  const [validationError, setValidationError] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [mobile, setMobile] = useState("");
+  const [ajaxList, setAjaxList] = useState([]);
 
-  const [userList, setUserList] = useState([]);
+  const makeAjaxCall = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    const response = await fetch(url);
+    const result = await response.json();
 
-  const handleUsername = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleMobile = (e) => {
-    setMobile(e.target.value);
-  };
-
-  const registerUser = () => {
-    // alert(`${username} ${password} ${email} ${mobile}`);
-    // TODO  MAKE AJAX CALL
-    // THERE IS NO DOM
-    if (username == "" || password == "" || email == "" || mobile == "") {
-      setValidationError(true);
-      return;
-    }
-
-    const user = {
-      username: username,
-      password: password,
-      email: email,
-      mobile: mobile,
-    };
-    const newList = [user, ...userList];
-    setUserList(newList);
-
-    // Clear in the End
-    setUsername("");
-    setPassword("");
-    setEmail("");
-    setMobile("");
-
-    setValidationError(false);
+    setAjaxList(result);
   };
 
   return (
     <div>
-      <h1>Working with Registration</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter username"
-          value={username}
-          onChange={handleUsername}
-          className={
-            username == "" && validationError ? "border border-danger" : ""
-          }
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter Password"
-          value={password}
-          onChange={handlePassword}
-          className={
-            password == "" && validationError ? "border border-danger" : ""
-          }
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter Email"
-          value={email}
-          onChange={handleEmail}
-          className={
-            email == "" && validationError ? "border border-danger" : ""
-          }
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter Mobile"
-          value={mobile}
-          onChange={handleMobile}
-          className={
-            mobile == "" && validationError ? "border border-danger" : ""
-          }
-        />
-      </div>
-      <div>
-        <input type="button" value="Register" onClick={registerUser} />
-      </div>
+      <h1>Hello React and Ajax</h1>
+      <input type="button" value="Make Ajax Call" onClick={makeAjaxCall} />
 
-      <hr />
-      {userList.map((item, index) => (
-        <div key={index}>
-          {item.username}, {item.password}, {item.email}, {item.mobile}
+      {ajaxList.map((item, index) => (
+        <div className="alert alert-primary" key={index}>
+          {item.title}
         </div>
       ))}
     </div>
