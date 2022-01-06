667fd18e6d508baa232f88bc976642c5b911775e
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-06 13:14:17 +0530

    react.

--- src/App.js
+++ src/App.js
@@ -9,114 +9,46 @@ export default function App() {
 }
 
 function MyComponent() {
-  const [validationError, setValidationError] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [mobile, setMobile] = useState("");
 
-  const [userList, setUserList] = useState([]);
-
-  const handleUsername = (e) => {
+  const handleUsernameChange = (e) => {
     setUsername(e.target.value);
   };
-
-  const handlePassword = (e) => {
+  const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
 
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleMobile = (e) => {
-    setMobile(e.target.value);
-  };
-
-  const registerUser = () => {
-    // alert(`${username} ${password} ${email} ${mobile}`);
-    // TODO  MAKE AJAX CALL
-    // THERE IS NO DOM
-    if (username == "" || password == "" || email == "" || mobile == "") {
-      setValidationError(true);
-      return;
-    }
-
-    const user = {
-      username: username,
-      password: password,
-      email: email,
-      mobile: mobile,
-    };
-    const newList = [user, ...userList];
-    setUserList(newList);
-
-    // Clear in the End
-    setUsername("");
-    setPassword("");
-    setEmail("");
-    setMobile("");
-
-    setValidationError(false);
+  const addUser = () => {
+    console.log(username, password);
   };
 
   return (
     <div>
-      <h1>Working with Registration</h1>
+      <h1>User Registration</h1>
       <div>
         <input
           type="text"
-          placeholder="Enter username"
+          name=""
+          id=""
           value={username}
-          onChange={handleUsername}
-          className={
-            username == "" && validationError ? "border border-danger" : ""
-          }
+          onChange={handleUsernameChange}
+          placeholder="Enter Username"
         />
       </div>
       <div>
         <input
           type="text"
-          placeholder="Enter Password"
+          name=""
+          id=""
           value={password}
-          onChange={handlePassword}
-          className={
-            password == "" && validationError ? "border border-danger" : ""
-          }
+          onChange={handlePasswordChange}
+          placeholder="Enter Password"
         />
       </div>
       <div>
-        <input
-          type="text"
-          placeholder="Enter Email"
-          value={email}
-          onChange={handleEmail}
-          className={
-            email == "" && validationError ? "border border-danger" : ""
-          }
-        />
+        <input type="button" name="" value="Register" onClick={addUser} />
       </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter Mobile"
-          value={mobile}
-          onChange={handleMobile}
-          className={
-            mobile == "" && validationError ? "border border-danger" : ""
-          }
-        />
-      </div>
-      <div>
-        <input type="button" value="Register" onClick={registerUser} />
-      </div>
-
-      <hr />
-      {userList.map((item, index) => (
-        <div key={index}>
-          {item.username}, {item.password}, {item.email}, {item.mobile}
-        </div>
-      ))}
     </div>
   );
 }
